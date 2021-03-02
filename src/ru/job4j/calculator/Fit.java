e9c82fbd6b3a7fb9318cb3aa96152f75481d65e8
NDreKru <ndrekrukru@gmail.com>
2021-03-02 19:13:31 +0500

    6. Идеальный вес [#482978]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -4,7 +4,6 @@ public class Fit {
     public static double manWeight(short height) {
         return (height - 100) * 1.15;
     }
-
     public static double womanWeight(short height) {
         return (height - 110) * 1.15;
     }
