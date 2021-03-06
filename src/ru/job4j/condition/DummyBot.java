94ab11a781bf8145412f245d7d3030d11bda69f9
NDre <ndrekrukru@gmail.com>
2021-03-06 09:54:31 +0500

    4.1.2. Глупый бот [#482921]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -11,9 +11,8 @@ public class DummyBot {
         return rsl;
     }
 
-
     public static void main(String[] args) {
-        String rsl = DummyBot.answer("Привет, Бот");
+        String rsl = DummyBot.answer("Привет, Бот.");
         System.out.println(rsl);
         rsl = DummyBot.answer("Пока.");
         System.out.println(rsl);
