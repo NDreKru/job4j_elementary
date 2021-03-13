83449dbb23a2767d11b679601430d472f521f339
NDreKru <ndrekrukru@gmail.com>
2021-03-13 09:22:00 +0500

    6.2.3. Перевернуть массив. [#482962]

---
+++ test/ru/job4j/array/TurnTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TurnTest {
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {4, 1, 6, 2};
+        int[] result = Turn.back(input);
+        int[] expect = new int[] {2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {5, 7, 1, 9, 3};
+        int[] result = Turn.back(input);
+        int[] expect = new int[] {3, 9, 1, 7, 5};
+        assertThat(result, is(expect));
+    }
+}
