0c9624cfc544277b591889842a61722d36f130e4
NDreKru <ndrekrukru@gmail.com>
2021-03-13 13:58:36 +0500

    6.5. Поиск индекса в диапазоне. [#482952]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -18,11 +18,44 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whenArrayHasnt7ThenMin1(){
+    public void whenArrayHasnt7ThenMin1() {
         int[] input = {5, 4, 3, 2};
         int value = 7;
         int result = FindLoop.indexOf(input, value);
         int expect = -1;
         Assert.assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind5() {
+        int[] input = {2, 6, 7, 3, 8, 3, 5};
+        int start = 4;
+        int finish = 6;
+        int value = 3;
+        int rst = FindLoop.indexOf(input, value, start, finish);
+        int expect = 5;
+        Assert.assertThat(rst, is(expect));
+    }
+
+    @Test
+    public void whenNotFound() {
+        int[] input = {2, 6, 7, 3, 8, 3, 5};
+        int start = 0;
+        int finish = 2;
+        int value = 3;
+        int rst = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        Assert.assertThat(rst, is(expect));
+    }
+
+    @Test
+    public void whenInvalidStart() {
+        int[] input = {2, 6, 7, 3, 8, 3, 5};
+        int start = -1;
+        int finish = 2;
+        int value = 3;
+        int rst = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        Assert.assertThat(rst, is(expect));
     }
 }
